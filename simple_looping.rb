ff6d6b5fcd15ea43c5a76f5bc819f953a2d45062
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-02 21:22:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -48,7 +48,7 @@ def until_iterator(number_of_times)
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  until number_of_times > 0 do
+  until number_of_times == 0 do
     puts phrase
     number_of_times -= 1
   end
